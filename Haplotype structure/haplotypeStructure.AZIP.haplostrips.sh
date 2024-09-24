cb0eba6e92e104f48bd8c1ee84bf46c17c10bc2e
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2024-09-24 18:10:18 +0200

    Add files via upload

--- Haplotype structure/haplotypeStructure.AZIP.haplostrips.sh
+++ Haplotype structure/haplotypeStructure.AZIP.haplostrips.sh
@@ -1,1 +1,10 @@
-dddddddddddd
+python haplostrips/haplostrips.py \
+      -v ASIP.NC_037340.1:63639821-63668127.vcf.gz \
+      -i NC_037340.1:63639821-63668127 \
+      -P pop.info \
+      -p Banteng,Gaur,Madura,Pasundan,Jabres,Pesisir,Aceh,EastAsianzebu,SumbaOngole,SouthAsianzebu,Europeantaurine \
+      -C '#9C755F','black','#59A14F','#E15759','#76B7B2','#F28E2B','#4E79A7','#EDC948','#B07AA1','#B3DE69','#BC80BD' \
+      -T \
+      -S 3 \
+      -t \
+      -o ASIP.NC_037340.1:63639821-63668127
