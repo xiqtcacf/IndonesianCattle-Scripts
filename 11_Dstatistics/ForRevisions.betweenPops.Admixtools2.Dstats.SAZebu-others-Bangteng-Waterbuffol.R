6666813421a273bebeaff6001c997d90e544e889
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2025-07-10 17:37:21 +0200

    Create ForRevisions.betweenPops.Admixtools2.Dstats.SAZebu-others-Bangteng-Waterbuffol.R

---
+++ 11_Dstatistics/ForRevisions.betweenPops.Admixtools2.Dstats.SAZebu-others-Bangteng-Waterbuffol.R
@@ -0,0 +1,11 @@
+library(admixtools)
+#library(tidyverse)
+
+pop1='SouthAsianzebu'
+pop2=c('Aceh','EastAsianzebu','Jabres','Madura','Pasundan','Pesisir','SumbaOngole','Bangladesh1','Bangladesh2','Bangladesh3','Dehong1','Dehong2','Jiangcheng1','Jiangcheng2','Kailali','Kapilvastu','Longlin1','Longlin2','Myanmar1','Myanmar2','Myanmar3','Saptari','Shigatse1','Shigatse2','Sri_Lanka1','Sri_Lanka2')
+pop3='Captivebanteng'
+pop4='waterbuffaloRef'
+
+out = qpdstat("/home/wlk579/2.0Indonesia_Bos_project/Revision_NC/EmuZebu_SouthernIndian/Dstatistics/pure.merge.140inds.waterbuffaloRef", pop1=pop1,pop2=pop2,pop3=pop3,pop4=pop4,unique_only = TRUE, auto_only = FALSE,f4mode = FALSE, sure = TRUE, blgsize = 5e6)
+#write_tsv(out, "/isdata/hellergrp/wlk579/Bos_project/5.Analyses/Admixtools2/betweenPops.SouthAsia-others-Banteng-waterbuffol.dstats.tsv")
+write.table(out, "/home/wlk579/2.0Indonesia_Bos_project/Revision_NC/EmuZebu_SouthernIndian/Dstatistics/pure.merge.140inds.waterbuffaloRef.waterbuffolH4.dstats.tsv", sep="\t", quote=FALSE, col.names = T, row.names = F)
